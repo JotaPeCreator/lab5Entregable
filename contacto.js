7e4f6618b362815dbfe37f4f0e2d50820732367b
JotaPeCreator <jotape.creator@outlook.com>
2025-09-05 00:41:40 -0500

    corregido

--- contacto.js
+++ contacto.js
@@ -2,8 +2,9 @@
 const form = document.getElementById('contactForm');
 form.addEventListener('submit', function(event) {
  const name = document.getElementById('name').value;
+ const motivo = document.getElementById('motivo').value;
  const email = document.getElementById('email').value;
- if (name === '' || email === '') {
+ if (name === '' || motivo === '' || email === '') {
  alert('Por favor, completa todos los campos.');
  event.preventDefault();
  } else {
